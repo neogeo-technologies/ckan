f1483f4c805b07874742de7b9d4efc9cd60fe6c2
m431m <4568458+m431m@users.noreply.github.com>
2020-04-16 08:55:39 +0200

    tag-name-validator with simple-quote support (#5)
    Added simple-quote in tag name validator method

--- ckan/logic/validators.py
+++ ckan/logic/validators.py
@@ -423,7 +423,7 @@ def tag_length_validator(value, context):
 
 def tag_name_validator(value, context):
 
-    tagname_match = re.compile('[\w \-.]*$', re.UNICODE)
+    tagname_match = re.compile("[\w\s\-.']*$", re.UNICODE)
     if not tagname_match.match(value):
         raise Invalid(_('Tag "%s" must be alphanumeric '
                         'characters or symbols: -_.') % (value))
