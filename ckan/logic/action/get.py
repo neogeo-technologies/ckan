aa774fcace9506ff739b111f8848860c3380404b
m431m <mmeliani@neogeo.fr>
2020-12-07 16:05:58 +0100

    Fixed mistake

--- ckan/logic/action/get.py
+++ ckan/logic/action/get.py
@@ -1998,7 +1998,7 @@ def package_search(context, data_dict):
             elif key == 'datatype':
                 # ADAPTER EN FONCTION DE LA PLATEFORME
                 if key_ == 'donnees-moissonnees':
-                    display_name = u"Données moissonées"
+                    display_name = u"Données moissonnées"
                 elif key_ == 'donnees-intelligentes':
                     display_name = u"Données intelligentes"
                 elif key_ == 'donnees-ouvertes':
